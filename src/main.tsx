6d750a317415d3edca7658660ab26cec6b101791
Basil Lowell <basillowell@yahoo.com>
2026-04-04 14:33:51 -0400

    Stop legacy datastore bootstrap on app load

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { initializeDataStore } from "./lib/dataStore";
 
-async function bootstrap() {
-  await initializeDataStore();
-  createRoot(document.getElementById("root")!).render(<App />);
-}
-
-void bootstrap();
+createRoot(document.getElementById("root")!).render(<App />);
